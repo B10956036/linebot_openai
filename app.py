99e203758a721d3184b428630dc9db670e7ad8e6
B10956036 <84556375+B10956036@users.noreply.github.com>
2023-08-17 03:33:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,22 +50,13 @@ def callback():
     return 'OK'
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-
+        
+        
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
