443579037decb0cdd4ef62672b1ecd416ccd18e3
B10956036 <84556375+B10956036@users.noreply.github.com>
2023-08-17 03:02:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,26 @@ line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
@@ -32,7 +41,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, video_message)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-
+#主程式
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
