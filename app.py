48bcca4dd6acfed9c5971b4c00d55e29990510f6
B10956036 <84556375+B10956036@users.noreply.github.com>
2023-08-17 03:21:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, video_message)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-        
+
 #主程式
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
