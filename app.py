1ce0d4a4d21046fd71c845e75cb58aa66b2b0e91
B10956036 <84556375+B10956036@users.noreply.github.com>
2023-08-17 02:46:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    message = event.message.text
+    if re.match('蛇丸',message):
+        video_message = VideoSendMessage(
+            original_content_url='https://i.imgur.com/XVmZmIE.mp4',
+        )
+        line_bot_api.reply_message(event.reply_token, video_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 import os
 if __name__ == "__main__":
