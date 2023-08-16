8d96b0e0d9beb2dee2e1e3c9e204797ef92cc641
B10956036 <84556375+B10956036@users.noreply.github.com>
2023-08-17 03:11:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
+import re
 
 app = Flask(__name__)
 
@@ -16,6 +17,24 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
