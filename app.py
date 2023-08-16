1d9cb43097c0e4ee1d721496c07c46f51e06bbe9
B10956036 <84556375+B10956036@users.noreply.github.com>
2023-08-16 23:46:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import time
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
-line_bot_api = LineBotApi(os.getenv('GUBTKGo9IN3dAiBMeiTCekl0urD7sKb5h03tDWxnLsoa73Ecl8RSVDlbfBlpWYFxpRgQMXXuleqnis+v94Mn8fQInMu1Bi1/aprLr+UMs/IW0QWQnF2u3UVBgM4MJZ4jyJ03dscimFa+jnPU1lgSZwdB04 t89/1O/w1cDnyilFU='))
+line_bot_api = LineBotApi(os.getenv('3XMzwMRo3nsij53LeDxRQp/sVC7OXS3D4HAdV2Cyslz6O50NlaQxzWarOU66W4wGpRgQMXXuleqnis+v94Mn8fQInMu1Bi1/aprLr+UMs/Kf8iK8NsgjYv5BF4CFkDPQFFL5/MW57mu6Uk4wR5iYFgdB04t89/1O/w1cDnyilFU='))
 # Channel Secret
 handler = WebhookHandler(os.getenv('518dde37ef19e2f9174d169a0e300bf8'))
 # OPENAI API Key初始化設定
