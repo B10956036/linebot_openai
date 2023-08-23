c49974f7ee3c8870341c150df3ec73276696f370
B10956036 <84556375+B10956036@users.noreply.github.com>
2023-08-23 20:57:02 +0800

    line傳送影片

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
 #======python的函數庫==========
-import json
-import tempfile, os, re, pyimgur, requests
+import tempfile, os, re, pyimgur, requests, json
 import datetime
 import openai
 import time
@@ -41,29 +35,25 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #
+    #imgur的CLIENT_ID
     CLIENT_ID = "9a88f1e57a71f9b"
-    PATH = "ch/image_0.jpg" #A Filepath to an image on your computer"
-    title = "image_0.jpg"
+    PATH = "ch/image_0.jpg"      #要改檔名
+    title = "image_0.jpg"        #要改檔名
     im = pyimgur.Imgur(CLIENT_ID)
+    #上傳圖片並取得網址
     uploaded_image = im.upload_image(PATH, title=title)
     
+    #上傳影片並取得網址
     import requests
     url = "https://api.imgur.com/3/upload"
-    payload = {'album': 'ALBUMID',
-    'type': 'file',
-    'disable_audio': '0'}
-    files = [
-      ('video', open('ch/output_1.mp4','rb'))
-    ]
-    headers = {
-      'Authorization': 'Bearer BEARERTOKENHERE'
-    }
+    payload = {'album': 'ALBUMID', 'type': 'file', 'disable_audio': '0'}
+    files = [('video', open('ch/output_1.mp4','rb'))]     #要改檔名
+    headers = {'Authorization': 'Bearer BEARERTOKENHERE'}
     response = requests.request("POST", url, headers=headers, data = payload, files = files)
     if response.status_code == 200:
-        # 解析 JSON 响应
+        # 解析 JSON 響應
         response_data = json.loads(response.text)
-        # 获取 "link" 值
+        # 獲取影片link
         link = response_data.get("data", {}).get("link")
         if link:
             print("Link:", link)
@@ -71,7 +61,7 @@ def handle_message(event):
             print("Link not found in the response.")
     else:
         print("Request failed with status code:", response.status_code)
-    #
+    #傳送訊息
     message = event.message.text
     if re.match('影片',message):
         video_message = VideoSendMessage(
